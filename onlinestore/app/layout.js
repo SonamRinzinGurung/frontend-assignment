ba21d2ac6bc400a9c733f3103850c205bb63b618
sonam_rinzin <sonamrinzingurung@gmail.com>
2023-07-22 11:55:50 +0545

    Build error fixes

--- onlinestore/app/layout.js
+++ onlinestore/app/layout.js
@@ -3,18 +3,14 @@ import "./globals.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { Header } from "@/components";
 
-export const metadata = {
-  title: "OnlineStore",
-  description: "React Intern Assignment",
-};
 const queryClient = new QueryClient(); // Create a client to be used by the QueryProvider
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <title>{metadata.title}</title>
-        <meta name="description" content={metadata.description} />
+        <title>"OnlineStore"</title>
+        <meta name="description" content={"React Intern Assignment"} />
       </head>
 
       <body>
